dee80e586433971f9df0f3bd5628da15e1c06ec9
Moriwam <mariwammimi99@gmail.com>
2023-11-22 23:52:58 +0600

    main

---
+++ template.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format="[%(ascitime)s] %(message)s:")
+
+
+project_name = "TextSummarizer"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/(project_name)/__init__.py",
+]
