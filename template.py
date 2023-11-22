657d63f6dae598d855902485279b60fb1787c51a
Moriwam <mariwammimi99@gmail.com>
2023-11-23 00:01:13 +0600

    Added templates

--- template.py
+++ template.py
@@ -11,4 +11,6 @@ list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
 ]
